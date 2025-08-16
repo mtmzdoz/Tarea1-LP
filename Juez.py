ac106340d2aaa3e0b80c8c3288f19dc9fce7034d
mtmzdoz <mmvasquezro@gmail.com>
2025-08-16 17:59:40 -0400

    Avance

--- Juez.py
+++ Juez.py
@@ -21,14 +21,49 @@ string = f"(?:{vocal}|{vocal_acentuada}|{letras})+"
 caracteres_permitidos = r"[¿?¡!.,;\-():\" \s]"
 palabra = f"(?:{string}|{caracteres_permitidos}|{digitos})"
 
-# Abrir el archivo en modo lectura
-EstrofasArchivo = open("estrofas.txt", "r", encoding="utf-8")
 
-# Leer todo el contenido
-ContenidoArchivo = EstrofasArchivo.read()
+def IdentificarRimas(nombre_archivo):
+    # Abrir el archivo en modo lectura
+    EstrofasArchivo = open(nombre_archivo, "r", encoding="utf-8")
+    
+    ContenidoArchivo = EstrofasArchivo.read()
+    
+    
+    Estrofas= ContenidoArchivo.strip().split("\n")
+    # Se detecta Palabras bonus fresco, mangos, melón, cielo. Linea 1
+    if "," in Estrofas[0] and Estrofas[1].strip()== "":
+        PalabrasBonus= Estrofas[0]
+        i=2
+    else:
+        i=0
+
+    ListaEstrofas = []
+    while i<len(Estrofas):
+        Verso=[]
+        if Estrofas[i] == "":
+            i+=1
+        while i < len(Estrofas) and Estrofas[i] != "":
+            Verso.append(Estrofas[i])
+            i+=1
+        ListaEstrofas.append(Verso)
+    
+    for verso in ListaEstrofas:
+        if len(verso) != 4:
+            print("Estrofa Invalida")
+        else:
+            print("Estrofa Valida")
+   
+    
+    
+    # Cerrar el archivo 
+    EstrofasArchivo.close()
+    return ListaEstrofas
+
+
 
 # Imprimir el contenido
 # print(ContenidoEstrofas)
+print(IdentificarRimas("estrofas.txt"))
 
-# Cerrar el archivo para liberar recursos
-EstrofasArchivo.close()
+
+
