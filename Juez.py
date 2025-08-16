dcfb273873c5d98af6650f906a10d1c09c1a50fe
mtmzdoz <mmvasquezro@gmail.com>
2025-08-16 14:52:45 -0400

    EBNF

--- Juez.py
+++ Juez.py
@@ -1,15 +1,34 @@
 #Importacion Modulo
 import re 
 
+'''
+EBNF General
+digitos ::= ([0-9])
+vocal ::= (AEIOU|aeiou)
+vocal_acentuada ::= (ÁÉÍÓÚ|áéíóú)
+letras ::= ([A-Z|a-z|Ññ])
+string ::= (<vocal>|<vocal_acentuada>|<letras>)
+caracteres_permitidos ::= (’¿’|’ ?’|’¡’|’ !’|’,’|’.’|’;’| ’ |’-’|’(’|’)’|’:’| ")
+palabra ::= (<string>|<caracteres_permitidos>|<digitos>)
+'''
+
+digitos = r"[0-9]"
+vocal = r"[AEIOUaeiou]"
+vocal_acentuada = r"[ÁÉÍÓÚáéíóú]"
+letras = r"[A-Za-zÑñ]"
+
+string = f"(?:{vocal}|{vocal_acentuada}|{letras})+"
+caracteres_permitidos = r"[¿?¡!.,;\-():\" \s]"
+palabra = f"(?:{string}|{caracteres_permitidos}|{digitos})"
 
 # Abrir el archivo en modo lectura
-Estrofastxt = open("estrofas.txt", "r", encoding="utf-8")
+EstrofasArchivo = open("estrofas.txt", "r", encoding="utf-8")
 
 # Leer todo el contenido
-ContenidoEstrofas = Estrofastxt.read()
+ContenidoArchivo = EstrofasArchivo.read()
 
 # Imprimir el contenido
-print(ContenidoEstrofas)
+# print(ContenidoEstrofas)
 
 # Cerrar el archivo para liberar recursos
-Estrofastxt.close()
+EstrofasArchivo.close()
--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 Nombre: Matilde Vásquez
 Rol: 202473652-3
 Versión Usada: Python 3.10.11 
+Observaciones:
+Las vocales tildadas si riman con las vocales no tildadas, es decir, por ejemplo, "melón" puede rimar con "melon".
