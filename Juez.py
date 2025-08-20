734a9fd55fd0c67094618a03aadd50845112581e
mtmzdoz <mmvasquezro@gmail.com>
2025-08-19 21:42:04 -0400

    gokuuuu

--- Juez.py
+++ Juez.py
@@ -32,13 +32,15 @@ def IdentificarEstrofas(nombre_archivo):
         PalabrasBonus= Estrofas[0]
         i=2
     else:
+        PalabrasBonus = "No hay"
         i=0
 
     ListaEstrofas = []
-    ValidezEstrofa= True
+    
 
     while i<len(Estrofas):
         Verso=[]
+        ValidezEstrofa= True
         if Estrofas[i] == "":
             i+=1
         while i < len(Estrofas) and Estrofas[i] != "":
@@ -52,8 +54,10 @@ def IdentificarEstrofas(nombre_archivo):
     EstrofasArchivo.close()
     return  PalabrasBonus, ListaEstrofas
 
-def ExtraerUltPalabraVerso(ListaEstrofas):
-    ListaUltPalVersos=[]
+def ExtraerUltPalabra(PalabrasBonus, ListaEstrofas):
+    UltPalVersos=[]
+    
+
     for versos, validez in ListaEstrofas:
         if len(versos) == 4:
             UltimasPalabras= []
@@ -61,18 +65,28 @@ def ExtraerUltPalabraVerso(ListaEstrofas):
                 palabras = palabra.strip().split()
                 ultima = palabras[-1].strip(',.´\'"')
                 UltimasPalabras.append(ultima)
-            ListaUltPalVersos.append(UltimasPalabras)
-    return ListaUltPalVersos
+
+            Bonus = 0
+            for ultima in UltimasPalabras:
+                if ultima in PalabrasBonus:
+                    Bonus = 2
+                    break 
+            
+            UltPalVersos.append((UltimasPalabras, Bonus))
+
+    return UltPalVersos
 
 def TipoRima(palabra1, palabra2):
     p1= palabra1
     p2= palabra2
     print(p1,p2)
 
-
+    Rima=[]
+    Puntaje=0
     #----GEMELA----
     if re.match(p1,p2):
-        return "Gemela"
+        Puntaje=1
+        return "Gemela", Puntaje
     
     #----CONSONANTE----
     # Rango desde la long completa de p1 hasta la 3ra letra, y empieza desde atras (-1)
@@ -82,89 +96,132 @@ def TipoRima(palabra1, palabra2):
             sufijo= concidencia.group()
             if re.search(sufijo, p2):
                 CantidadLetras=len(sufijo)
-                return "Consonante", CantidadLetras
+                if CantidadLetras <= 4:
+                    Puntaje = 5
+                else:
+                    Puntaje=8
+                Rima.append(("Consonante", Puntaje))
     
    #----ASONANTE----
     VocalesP1 = ""
     VocalesP2 = ""
-    for vocal in re.findall(vocales, p1):
+    for vocal in re.findall(vocales, p1.lower()):
         VocalesP1 += vocal
 
-    for vocal in re.findall(vocales, p2):
+    for vocal in re.findall(vocales, p2.lower()):
         VocalesP2 += vocal
 
-    print(VocalesP1, VocalesP2)
+    #print(VocalesP1, VocalesP2)
+
     VocalesIguales= ""
     ContadorVocales= 0
     for i in range(1, min(len(VocalesP1), len(VocalesP2)) + 1):
         if VocalesP1[-i]==VocalesP2[-i]:
             VocalesIguales= VocalesP1[-i] + VocalesIguales
             ContadorVocales +=1
-        else:
-            break
     
+    #print(VocalesIguales, ContadorVocales)
+
     if VocalesIguales and re.search(VocalesIguales, VocalesP2):
-        return "Asonante", CantidadLetras
+        if ContadorVocales == 1:
+            Puntaje=3
+        elif ContadorVocales == 2:
+            Puntaje= 4
+        elif ContadorVocales >= 3:
+            Puntaje=8
+        Rima.append(("Asonante", Puntaje))
+    
+    MejorRima= None
+    for rima in Rima:
+        if rima[0] in ("Consonante", "Asonante"):
+            if MejorRima is None or rima[1]> MejorRima[1]:
+                MejorRima = rima
+    
+    if MejorRima:
+        return MejorRima
+    
+    #----MISMA TERMINACIÓN----
+    for i in range(min(len(p1), len(p2)), 1, -1):
+        coincidencia= p1[-i:]
+        if re.search(coincidencia,p2):
+            CantidadLetras= len(coincidencia)
+            if CantidadLetras >= 2:
+                Puntaje=2
+            return "Misma Terminación", Puntaje
     
     
+
+    return "No Riman", 0
 
 
 def IDRimas(ListaUltPalabraVersos):
-    Puntaje= 0
-    for palabra in ListaUltPalabraVersos:
-        v1, v2, v3, v4 = palabra
-        print(v1, v2, v3, v4)
+    
+    Resultado= []
 
-      
+    for palabra, bonus in ListaUltPalabraVersos:
+        v1, v2, v3, v4 = palabra
+        #print(v1, v2, v3, v4)
 
         pares = [(v1,v2),(v1,v3),(v1,v4),(v2,v3),(v2,v4),(v3,v4)]
-        
-        for a,b in pares:
-            Tipo, CoincidenciaLetras = TipoRima(a,b)
-            if Tipo == "Consonante" and CoincidenciaLetras <= 4:
-                Puntaje +=5
-            else:
 
-                puntaje += 5
-            elif tipo == "asonante":
-                puntaje += 5
-            # ninguna → 0 pts
-            
-        Puntaje.append(puntaje)
+        TotalPuntaje= 0
+        RimaFinal= []
     
-    return Puntaje
+
+        for a,b in pares:
+            Tipo, Puntos = TipoRima(a,b)
+            print(Tipo, Puntos)
+            if Tipo != "No Riman":
+                TotalPuntaje += Puntos
+                Descuento = False
+            elif Tipo == "No Riman":
+                Descuento = True 
+            if Tipo not in RimaFinal:
+                RimaFinal.append(Tipo)
+        
+        if Descuento:
+            TotalPuntaje -=2
+
+        print(TotalPuntaje)
+        TotalPuntaje= (TotalPuntaje + bonus )/5
+        Resultado.append((RimaFinal, TotalPuntaje, bonus))
+
+        print (Resultado)
+    return Resultado
 
    
-    
-
-
 def ArchivoSalida(nombre_archivo):
 
     PalabrasBonus, ListaEstrofas = IdentificarEstrofas(nombre_archivo)
+    ListaUltPalabraVersos = ExtraerUltPalabra(PalabrasBonus, ListaEstrofas)
+    Resultado = IDRimas(ListaUltPalabraVersos)
+
     ArchSalida= "decision.txt"
     ArchSalida= open(ArchSalida,"w", encoding="utf-8")
 
     i=1
-    for verso, validez in  ListaEstrofas:
-        if len(verso) != 4 or validez == False:
-            escribir=f"Estrofa {i}: Inválida" 
-        else:
-            escribir=f"Estrofa {i}: Válida"
-        
-        ArchSalida.write(escribir+"\n")
-        i+=1         
-    
+    indice_resultado=0
+    for verso, validez in ListaEstrofas:
+        RimaFinal, TotalPuntos, Bonus = Resultado[indice_resultado]
 
-# Imprimir el contenido
-#print(IdentificarEstrofas("estrofas.txt"))
+        if len(verso) != 4 or validez == False:
+            ArchSalida.write("Estrofa {0}: Inválida\n".format(i))
+        else:
+            Ptos = f"{TotalPuntos:.1f}/10"
+            if  Bonus > 0:
+                ArchSalida.write("Estrofa {}: {} (BONUS)\n".format(i, Ptos))
+            else:
+                ArchSalida.write("Estrofa {}: {}\n".format(i, Ptos))
+            if RimaFinal:
+                ArchSalida.write("Rimas: " + ", ".join(RimaFinal) + "\n")
+            indice_resultado +=1
+        i+=1
+    ArchSalida.close()
+
 PalabrasBonus, ListaEstrofas = IdentificarEstrofas("estrofas.txt")
-#print(PalabrasBonus)
-#print(ListaEstrofas)
-ListaUltPalabraVersos= ExtraerUltPalabraVerso(ListaEstrofas)
-print(ListaUltPalabraVersos)
-#print(IdentificarRimas(ListaUltPalabraVersos))
-v1="malambo"
-v2="mangos"
+ListaUltPalabraVersos = ExtraerUltPalabra(PalabrasBonus, ListaEstrofas)
+v1="desierto"
+v2="tanto"
 print(TipoRima(v1,v2))
 print(ArchivoSalida("estrofas.txt"))
 
--- README.txt
+++ README.txt
@@ -1,5 +1,11 @@
 Nombre: Matilde Vásquez
 Rol: 202473652-3
-Versión Usada: Python 3.10.11 
+Rut: 21.715.078-7
+Versión Usada: Python 3.10.11 en Visual Studio Code
+
 Observaciones:
-Las vocales tildadas NO riman con las vocales no tildadas, es decir, por ejemplo, "melón" NO puede rimar con "melon".
+-No hay problemas de ejecutarlo sin palabras bonus
+-No hay problema de ejecutarlo con espacio en blanco al inicio o final
+-Las vocales tildadas NO RIMAN con las vocales no tildadas, es decir, por ejemplo, "melón" NO puede rimar con "melon".
+
+
